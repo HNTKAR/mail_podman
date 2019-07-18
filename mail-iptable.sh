ae25a3e26a4ae4c9c3b73c6aea6704da7afb5a60
kusari-k <s15426@salesio-sp.ac.jp>
2019-07-18 23:06:58 +0900

    USER-MAIL-CHAIN is 1st rule

--- mail-iptable.sh
+++ mail-iptable.sh
@@ -7,4 +7,4 @@ iptables -I USER-MAIL-CHAIN -j ACCEPT -m set --match-set white-list src -p tcp -
 iptables -I USER-MAIL-CHAIN -j ACCEPT -m set --match-set white-list src -p tcp --dport 993
 iptables -I USER-MAIL-CHAIN -j ACCEPT -m set --match-set white-list src -p tcp --dport 587
 iptables -I USER-MAIL-CHAIN -j ACCEPT -m set --match-set white-list src -p tcp --dport 465
-iptables -I DOCKER-USER 2 -j USER-MAIL-CHAIN
+iptables -I DOCKER-USER -j USER-MAIL-CHAIN
