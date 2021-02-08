26e53349111b385f93d0a81dbaad17c16fbaf246
kusari-k <45356209+kusari-k@users.noreply.github.com>
2021-02-09 03:23:11 +0900

    use postconf -P

--- run-postfix.sh
+++ run-postfix.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
+mkdir -p -m 644 /log/log /log/postfix_log /conf
+
 #start rsyslog
 rsyslogd
 
 #start postfix
 /usr/libexec/postfix/aliasesdb
 /usr/libexec/postfix/chroot-update
-/usr/sbin/postfix start
+/usr/sbin/postfix -c /conf start
 
 tail -f /dev/null
